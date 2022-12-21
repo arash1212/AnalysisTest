224a4e98fccb594ecc284c7a50d4373caa5a96ce
Arash <arashsalehi849@yahoo.com>
2022-12-21 14:16:09 +0330

    Initial commit

---
+++ src/main/java/com/example/salehi/SalehiApplication.java
@@ -0,0 +1,13 @@
+package com.example.salehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalehiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalehiApplication.class, args);
+    }
+
+}
